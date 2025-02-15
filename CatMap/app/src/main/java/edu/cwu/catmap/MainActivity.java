ea1044d2c4aaab6c3b8f64e229f702227278dce5
sillaustin <115026072+sillaustin@users.noreply.github.com>
2025-02-14 19:57:28 -0800

    Update MainActivity.java
    added map functionality

--- CatMap/app/src/main/java/edu/cwu/catmap/MainActivity.java
+++ CatMap/app/src/main/java/edu/cwu/catmap/MainActivity.java
@@ -8,17 +8,25 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap gMap;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.id_map);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        LatLng location = new LatLng(47.0076653, -120.5366559);
+        googleMap.addMarker(new MarkerOptions().position(location).title("CWU"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
     }
 }
