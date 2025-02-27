6a1ecb3567affb8742f197ee56a7ad86c0c31df7
Eli <158611286+CapybarasUnited@users.noreply.github.com>
2025-02-27 01:50:30 -0800

    Began working on location info page
    need to add the rounded background to the locations page, use card view?

---
+++ CatMap/app/src/main/java/edu/cwu/catmap/activities/LocationInformationActivity.java
@@ -0,0 +1,26 @@
+package edu.cwu.catmap.activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import edu.cwu.catmap.R;
+
+public class LocationInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_location_information);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
