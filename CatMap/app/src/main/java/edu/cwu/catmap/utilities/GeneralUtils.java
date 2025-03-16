2912d884813f5d541167669bbaad04c0155565b4
Eli <158611286+CapybarasUnited@users.noreply.github.com>
2025-03-16 03:38:47 -0700

    uhh...

--- CatMap/app/src/main/java/edu/cwu/catmap/utilities/GeneralUtils.java
+++ CatMap/app/src/main/java/edu/cwu/catmap/utilities/GeneralUtils.java
@@ -2,17 +2,14 @@ package edu.cwu.catmap.utilities;
 import android.content.Context;
 import android.content.SharedPreferences;
 
-import androidx.preference.Preference;
-import androidx.preference.PreferenceManager;
-
 import edu.cwu.catmap.R;
 
-public class GeneralUtils {
+public class generalUtils {
     public static int getSavedTheme(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences(Constants.KEY_SHARED_PREFRENCES_NAME, Context.MODE_PRIVATE);
-        boolean isDarkMode = prefs.getBoolean(Constants.KEY_SHARED_PREFRENCES_DARK_MODE, false); // Default to light theme
+        SharedPreferences prefs = context.getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
+        String theme = prefs.getString("theme", "light"); // Default to light theme
 
-        if (isDarkMode) {
+        if (theme.equals("dark")) {
             return R.style.Theme_CatMap_Dark; // Use dark theme
         } else {
             return R.style.Theme_CatMap; // Use light theme
